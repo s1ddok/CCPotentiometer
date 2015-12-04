0b962b0dee61c0c2c9da4a05b24db5c87891dc3b
s1ddok <s1ddok@macbook-pro-andrey.local>
2015-12-05 02:14:46 +0300

    Initial Commit

---
+++ CCPotentioMeterDemo/Classes/AppDelegate.h
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.h
+//
+//  Created by : Andrey Volodin
+//  Project    : CCPotentioMeterDemo
+//  Date       : 05.12.15
+//
+//  Copyright (c) 2015 Andrey Volodin.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+// -----------------------------------------------------------------------
+
+@interface AppDelegate : CCAppDelegate
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CCPotentioMeterDemo/Classes/HelloWorldScene.h
@@ -0,0 +1,61 @@
+//
+//  HelloWorldScene.h
+//
+//  Created by : Andrey Volodin
+//  Project    : CCPotentioMeterDemo
+//  Date       : 05.12.15
+//
+//  Copyright (c) 2015 Andrey Volodin.
+//  All rights reserved.
+//
+// -----------------------------------------------------------------
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
+- (instancetype)init;
+
+// -----------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
